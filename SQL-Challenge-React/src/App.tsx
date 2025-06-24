658b025edec3bf0f01edf433d37323dcbcaa60e1
Rafe Milner <milnerrafe@outlook.com>
2025-06-24 08:34:44 +0800

    Added start points for the Challenge

---
+++ SQL-Challenge-React/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import { Button } from "@carbon/react";
+
+function Testbutton() {
+  return <Button>Example usage</Button>;
+}
+
+function App() {
+  return (
+    <>
+      <Testbutton></Testbutton>
+    </>
+  );
+}
+
+export default App;
