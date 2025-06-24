c4620d3f6e892dbf3dec3709a7eb64b773b7a951
Rafe Milner <milnerrafe@outlook.com>
2025-06-24 11:28:35 +0800

    add pydb and makedb

--- SQL-Challenge-Python-Server/main.py
+++ SQL-Challenge-Python-Server/main.py
@@ -1,6 +1,86 @@
-def main():
-    print("Hello from adam")
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory, render_template_string, jsonify
+import mysql.connector
+import sqlparse
+import sqlite3
+
+
+def startuppythondb():
+    con = sqlite3.connect("python.db")
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS maindb(name TEXT, data TEXT)")
+    con.commit()
+    con.close()
+
+def mysqlinfo(type):
+    con = sqlite3.connect("python.db")
+    cur = con.cursor()
+    res = cur.execute(f"SELECT data FROM maindb WHERE name='mysql{type}'")
+    row = res.fetchone()
+    con.close()
+    return row[0] if row else None
+
+def makedb():
+    conn = mysql.connector.connect(
+        host=mysqlinfo('host'),
+        user=mysqlinfo('user'),
+        password=mysqlinfo('password'),
+    )
+
+    cursor = conn.cursor()
+
+    stmt = 'CREATE DATABASE IF NOT EXISTS sqlchallengedb;'
+
+    try:
+        cursor.execute(stmt)
+    except mysql.connector.Error as err:
+        print(f"Error executing statement:\n{stmt}\n\nMySQL Error: {err}")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+app = Flask(__name__)
+
+
+@app.route('/api')
+def index():
+    return 'hello world'
+
+
+@app.route('/api/setup')
+def setup():
+
+    json_data = request.get_json()
+
+    host = json_data.get('host')
+    username = json_data.get('username')
+    password = json_data.get('password')
+
+    con = sqlite3.connect("python.db")
+    cur = con.cursor()
+    cur.execute(f"""
+        INSERT INTO maindb
+        VALUES (mysqlhost, {host},);
+        INSERT INTO maindb
+        VALUES (mysqlusername, {username},);
+        INSERT INTO maindb
+        VALUES (mysqlpassword, {password},);
+        """)
+    con.commit()
+    con.close()
+
+
+    if not json_data:
+        return jsonify({'error': 'No JSON received'}), 400
+
+    makedb()
+    return 'hello world'
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    startuppythondb()
+    app.run(host='0.0.0.0', port=8000)
